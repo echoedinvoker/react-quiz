e27ead549abf0452b02a9bc42d9dec2071d3b442
matt <echoedinvoker@gmail.com>
2023-06-24 12:42:00 +0800

    16-190

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import DateCounter from "./DateCounter"
+import Headers from "./Header"
+import Main from "./Main"
+
 
 export default function App() {
   return <div>
-    <DateCounter />
+    <Headers />
+    <Main>
+      <p>1/15</p>
+      <p>question</p>
+    </Main>
   </div>
 }
